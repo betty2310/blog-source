6d6508bdbd8d07a0877ca3202797b7af562e0db1
betty2310 <huynh.dh2310@gmail.com>
2022-04-04 23:20:34 +0700

    ✨ Feat:

--- utils/consts.ts
+++ utils/consts.ts
@@ -1,3 +1,3 @@
-export const ENABLED_PROJECTS = ['everyday', 'st' ];
+export const ENABLED_PROJECTS = ['everyday', 'st', "dotfiles" ];
 export const SITE_URL = 'https://betty.rocks';
 export const SITE_NAME = '/home/betty';
